981ae1c52620d6809f129c9abd4cc0fbcd39a9d5
kissmygritts <kissmygritts08@gmail.com>
2016-08-22 12:34:42 -0700

    user auth

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express         = require('express'),
     bodyParser      = require('body-parser'),
     mongoose        = require('mongoose'),
     methodOverride  = require("method-override"),
+    passport        = require('passport'),
+    LocalStrategy   = require('passport-local'),
+    User            = require('./models/user')
     seedDB          = require('./seed_db'),
     app             = express();
 
@@ -11,23 +14,34 @@ var indexRoutes = require('./routes/index'),
     checkinRoutes = require('./routes/checkins');
 
 // APP CONFIG
-mongoose.connect("mongodb://localhost/occurences");
+mongoose.connect("mongodb://localhost/ndow-forms");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 seedDB();
 
+// AUTHENTICATION PASSPORT
+app.use(require("express-session")({
+    secret: "Super secret testing secret",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
+
 // ROUTES
 app.use("/", indexRoutes);
 app.use("/observations", observationRoutes);
 app.use("/checkins", checkinRoutes);
-
-// // BIGHORN ROUTES ==============================================================
-// // New, GET - go to form to enter bighorn checking
-// app.get('/bighorncheckin/new', function(req, res){
-//   res.render('bighornCheckout');
-// });
 
 // LISTEN, SERVER
 app.listen(3000, function(){
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
-var express = require("express"),
-    router = express.Router();
+var express = require('express'),
+    router = express.Router(),
+    passport = require('passport'),
+    User = require('../models/user');
 
-
+// show landing page
 router.get('/', function(req, res) {
     res.render('index');
 });
+
+// show user register form
+router.get('/register', function(req, res){
+    res.render('register');
+});
+
+// handle registration logic
+router.post('/register', function(req, res){
+    var newUser = new User({
+        username: req.body.username,
+        first: req.body.first,
+        last: req.body.last,
+        division: req.body.division,
+        permissions: 'admin'
+    });
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log('err')
+            return res.redirect('register');
+        }
+        passport.authenticate('local')(req, res, function(){
+            res.redirect('/');
+        });
+    });
+});
+
+// show login page
+router.get('/login', function(req, res){
+    res.send('register page');
+});
+
+// handle login logic
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/'
+}), function(req, res){
+});
+
+// logout route
+router.get('/logout', function(req, res){
+    req.logout();
+    res.redirect("/")
+})
 
 module.exports = router;
