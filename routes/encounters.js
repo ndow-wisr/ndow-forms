8435bd0ef8126d7572b71907be2eb3ac1d2e3070
kissmygritts <kissmygritts08@gmail.com>
2016-09-19 18:40:40 -0700

    basic create functionality for encounters

--- routes/encounters.js
+++ routes/encounters.js
@@ -2,26 +2,26 @@ var express = require('express'),
     router = express.Router(),
     models = require('../models');
 
-models.Animal.create({
-    source: 'wildife health app',
-    species_id: 1,
-    user_id: 1,
-    sex: 'female',
-    field_id: 20000,
-    Encounters: [
-        {
-            status: 'alive',
-            enc_date: '2016-10-10',
-            enc_method: 'capture',
-            enc_reason: 'disease',
-            biologist: 'mitchell gritts'
-        }
-    ]
-}, {
-    include: [{
-        model: models.Encounter
-    }]
-});
+// models.Animal.create({
+//     source: 'wildife health app',
+//     species_id: 1,
+//     user_id: 1,
+//     sex: 'female',
+//     field_id: 20000,
+//     Encounters: [
+//         {
+//             status: 'alive',
+//             enc_date: '2016-10-10',
+//             enc_method: 'capture',
+//             enc_reason: 'disease',
+//             biologist: 'mitchell gritts'
+//         }
+//     ]
+// }, {
+//     include: [{
+//         model: models.Encounter
+//     }]
+// });
 
 // index, get, list all encounters
 router.get('/', function(req, res){
@@ -33,7 +33,8 @@ router.get('/', function(req, res){
             {model: models.Species,
              attributes: ['species_name', 'common_name', 'id']},
              {model: models.Encounter}
-        ]
+        ],
+        order: [ [ 'created_at', 'DESC' ] ]
     })
     .then(function(animals){
         res.status(200).json(animals);
@@ -47,42 +48,34 @@ router.get('/new', function(req, res){
 
 // create, post, create a new encounter
 router.post('/', function(req, res){
-    // var dat = req.body
-    // console.log(JSON.stringify(dat));
-    var enc = {
-        status: req.body.status,
-        enc_date: req.body.date,
-        reencounter: req.body.reencounter,
-        relocated: req.body.relocated
-    };
-    var animal = {
-        sex: req.body.sex,
-        field_id: req.body.species_id,
-        species_id: 1
-    };
-    models.Animal.create({
-        source: 'wildife health app',
-        species_id: 1,
-        user_id: 1,
-        sex: 'female',
-        field_id: 20000,
-        encounters: [
-            {
-                status: 'alive',
-                enc_date: '2016-10-10',
-                enc_method: 'capture',
-                enc_reason: 'disease',
-                biologist: 'mitchell gritts'
-            }
-        ]
-    }, {
+    var dat = req.body;
+    console.log(JSON.stringify(dat, null, '\t'));
+    console.log(req.body.animal.species_id);
+    var animal = req.body.animal;
+    animal.Encounters = req.body.encounter;
+
+    models.Animal.create(animal, {
         include: [{
-            model: models.Encounter,
-            as: 'encounters'
+            model: models.Encounter
         }]
     }).then(function(){
-        res.redirect('/');
+        res.redirect('/encounters');
     });
+
+    // models.Animal.create({
+    //     source: 'wildlife health',
+    //     species_id: req.body.animal[species_id],
+    //     user_id: 1,
+    //     sex: req.body.sex,
+    //     field_id: req.body.field_id,
+    //     Encounters: [ req.body.encounter ]
+    // }, {
+    //     include: [{
+    //         model: models.Encounter
+    //     }]
+    // }).then(function(){
+    //     res.redirect('/encounters');
+    // });
 });
 
 module.exports = router;
