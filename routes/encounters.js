2e82c0f748efa8b08c3e485b28575d0b2944cf32
kissmygritts <kissmygritts08@gmail.com>
2016-10-15 19:32:34 -0700

    field names

--- routes/encounters.js
+++ routes/encounters.js
@@ -27,44 +27,44 @@ router.get('/new', function(req, res){
 
 // create, post, create a new encounter
 router.post('/', function(req, res){
-    // console.log(JSON.stringify(req.body, null, '\t'));
+    console.log(JSON.stringify(req.body, null, '\t'));
 
-    var animal = req.body.animal;
-    var encounter = req.body.enc;
-    var location = req.body.loc;
-    var marks = parseDynamicContent(req.body.mark);
-    var biometric = parseDynamicContent(req.body.biom);
-
-    // if (encounter.marks == 'yes') {
-    //     var marks = req.body.mark;
-    //     Object.keys(marks).forEach(function(key) {
-    //         if (marks[key] == '') {
-    //             delete marks[key];
+    // var animal = req.body.animal;
+    // var encounter = req.body.enc;
+    // var location = req.body.loc;
+    // var marks = parseDynamicContent(req.body.mark);
+    // var biometric = parseDynamicContent(req.body.biom);
+    //
+    // // if (encounter.marks == 'yes') {
+    // //     var marks = req.body.mark;
+    // //     Object.keys(marks).forEach(function(key) {
+    // //         if (marks[key] == '') {
+    // //             delete marks[key];
+    // //         }
+    // //     });
+    // //     animal.Marks = [ marks ];
+    // // };
+    //
+    // encounter.Location = location;
+    // encounter.Biometrics = biometric;
+    // animal.Encounters = [ encounter ];
+    // animal.Marks = marks;
+    // console.log(JSON.stringify(animal, null, '\t'));
+    //
+    // models.Animal.create(animal, {
+    //     include: [
+    //         { model: models.Mark },
+    //         {
+    //             model: models.Encounter,
+    //             include: [
+    //                 { model: models.Location },
+    //                 { model: models.Biometric }
+    //             ]
     //         }
-    //     });
-    //     animal.Marks = [ marks ];
-    // };
-
-    encounter.Location = location;
-    encounter.Biometrics = biometric;
-    animal.Encounters = [ encounter ];
-    animal.Marks = marks;
-    console.log(JSON.stringify(animal, null, '\t'));
-
-    models.Animal.create(animal, {
-        include: [
-            { model: models.Mark },
-            {
-                model: models.Encounter,
-                include: [
-                    { model: models.Location },
-                    { model: models.Biometric }
-                ]
-            }
-        ]
-    }).then(function(){
+    //     ]
+    // }).then(function(){
         res.redirect('/encounters/new');
-    });
+    // });
 
     // var animal = req.body.animal;
     // var encounter = req.body.encounter;
